a09f1591b75266dbbdb111717382db5c88d9d51e
ManellDmitry <manell@ukr.net>
2022-08-02 12:18:11 +0300

    add icons sprite

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React from "react"
-import Footer from "./component/Footer"
-import Header from "./component/Header"
+import React from "react";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+import { Button, Link, IconButton } from "./component/Button";
+import styled from "styled-components";
 
-const navLinks= [
-  {id:"1",label: "Our work"},
-  {id:"2",label: "What we do"},
-  {id:"3",label: "About us"},
-  {id:"4",label: "Get in touch"}
-]
+const navLinks = [
+  { id: "1", label: "Our work" },
+  { id: "2", label: "What we do" },
+  { id: "3", label: "About us" },
+  { id: "4", label: "Get in touch" },
+];
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  align-items: center;
+`;
 
 function App() {
-  return(
+  const handleButtonClick = () => {};
+  const handleLinkButtonClick = () => {};
+
+  return (
     <div className="App">
-     <Header links={navLinks}/>
-      <Footer/>
+      <Header links={navLinks} />
+
+      <ButtonsWrapper>
+        <Button label="see all works" handleClick={handleButtonClick} />
+        <Link label="get in touch" handleClick={handleLinkButtonClick} />
+        <IconButton iconId="close" />
+      </ButtonsWrapper>
+
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,24 +1,41 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-const List= styled.ul`
-display: flex;
-`
-const Item= styled.li`
-margin-right: 30px;
-`
+const List = styled.ul`
+  display: flex;
+`;
 
-function Navigation({links}){
-    return(
-        <nav>
-        <List>
-          {links && links.map(({id,label}) => 
-            (<Item key={id}>
-                  <a href="/">{label}</a>
-            </Item>)    
-          )}
-        </List>
-      </nav>
-    )
+const Item = styled.li`
+  margin-right: 30px;
+`;
+
+function Navigation({ links }) {
+  const handleItemClick = () => {};
+
+  const handleLinkClick = (e) => {
+    e.preventDefault();
+  };
+  const handleLinkMouseEnter = (e) => {};
+
+  return (
+    <nav>
+      <List>
+        {links &&
+          links.map(({ id, label, path }) => (
+            <Item key={id} onClick={handleItemClick}>
+              <Link
+                to={`/${path}`}
+                onClick={handleLinkClick}
+                onMouseEnter={handleLinkMouseEnter}
+              >
+                {label}
+              </Link>
+            </Item>
+          ))}
+      </List>
+    </nav>
+  );
 }
-export default Navigation
+
+export default Navigation;
